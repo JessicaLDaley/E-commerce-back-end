ede9324c46a955bec024e219418b906069b1e471
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-10-26 14:58:45 -0400

    Created models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: "category_id",
+
+});
 
 // Categories have many Products
+Caregory.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tag, {
+  foreignKey: "product_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+})
 
 module.exports = {
   Product,
