41b576330beea14480c1907e6a98a9ca7afab423
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-11-19 13:46:13 -0500

    fixed typo errors

--- models/index.js
+++ models/index.js
@@ -5,30 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Products.belongsTo(Category, {
-  foreignKey: "category_id",
 
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
 });
 
 // Categories have many Products
-Caregory.hasMany(Product, {
-  foreignKey: "category_id"
+
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
-Products.belongsToMany(Tag, {
-  foreignKey: "product_id"
+
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tags.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: "tag_id"
-})
+
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: 'tag_id'
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
